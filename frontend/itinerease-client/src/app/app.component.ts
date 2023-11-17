6b3f0d3cf917c48e38d2097b162f9aae86bb9d7c
sandru dairus <darius_sandru@yahoo.com>
2023-11-17 19:00:22 +0200

    backup

--- frontend/itinerease-client/src/app/app.component.ts
+++ frontend/itinerease-client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ModalsComponent } from './modals/modals.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [CommonModule, RouterOutlet, ModalsComponent]
 })
 export class AppComponent {
   title = 'itinerease-client';
