508e9d2a67b124d7753fa243fee371b1d6aeda40
sandru dairus <darius_sandru@yahoo.com>
2023-11-17 22:33:47 +0200

    update model

--- frontend/itinerease-client/src/app/app.component.ts
+++ frontend/itinerease-client/src/app/app.component.ts
@@ -1,27 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
+
 import { NgbAlertModule, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { ContainerComponent } from "./container/container.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet,NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe]
-=======
-import { ContainerComponent } from './container/container.component';
-
-@Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
->>>>>>> main
+    imports: [CommonModule, RouterOutlet, NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe, ContainerComponent]
 })
 export class AppComponent {
   title = 'itinerease-client';
