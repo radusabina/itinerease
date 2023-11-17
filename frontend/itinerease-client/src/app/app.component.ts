31b12d7ec1bf6d19fce397bee143550fb213cd0a
sandru dairus <darius_sandru@yahoo.com>
2023-11-17 22:00:30 +0200

    update model

--- frontend/itinerease-client/src/app/app.component.ts
+++ frontend/itinerease-client/src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ModalsComponent } from './modals/modals.component';
+import { NgbAlertModule, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
-    imports: [CommonModule, RouterOutlet, ModalsComponent]
+    imports: [CommonModule, RouterOutlet,NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe]
 })
 export class AppComponent {
   title = 'itinerease-client';
+  dataStartModel!: NgbDateStruct;
+  dateEndModel!: NgbDateStruct;
 }
+
+
+
+
--- frontend/itinerease-client/src/app/modals/modals.component.ts
+++ frontend/itinerease-client/src/app/modals/modals.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NgbAlertModule, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-modals',
-  standalone: true,
-  imports: [CommonModule],
+  standalone: true,    
+  imports: [CommonModule, RouterOutlet,NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe],
   templateUrl: './modals.component.html',
   styleUrl: './modals.component.scss'
 })
 export class ModalsComponent {
-
+  title = 'itinerease-client';
+  dataStartModel!: NgbDateStruct;
+  dateEndModel!: NgbDateStruct;
 }
