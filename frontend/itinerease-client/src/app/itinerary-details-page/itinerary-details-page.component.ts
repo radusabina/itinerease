c6301bcef01e0b0463916c943598dd3ba378b154
tudorsuiu <tudorsuiu21@gmail.com>
2023-11-26 23:24:22 +0200

    saved changes

--- frontend/itinerease-client/src/app/itinerary-details-page/itinerary-details-page.component.ts
+++ frontend/itinerease-client/src/app/itinerary-details-page/itinerary-details-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgbDateStruct, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-itinerary-details-page',
@@ -12,20 +13,34 @@ import { NgbDateStruct, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ItineraryDetailsPageComponent {
     itineraryName: string = 'Itinerary name test';
-    startDate: NgbDateStruct = {year: 2023, month: 11, day: 23};
-    endDate: NgbDateStruct = {year: 2023, month: 11, day: 30};
+    startDate: NgbDateStruct = { year: 2023, month: 11, day: 23 };
+    endDate: NgbDateStruct = { year: 2023, month: 11, day: 30 };
     budget: number = 6969;
     numberOfPersons: number = 5;
-    whereFromCountry: string = "Germania";
-    whereFromCity: string = "Berlin";
-    whereToCountry: string = "Spania";
-    whereToCity: string = "Barcelona";
-    
+    whereFromCountry: string = 'Germania';
+    whereFromCity: string = 'Berlin';
+    whereToCountry: string = 'Spania';
+    whereToCity: string = 'Barcelona';
+    transportType: string = 'Bus';
+    transportPrice: number = 15;
+    accommodationName: string = 'Hotel Ratatouille';
+    accommodationAddress: string = 'Adresa luceafarului 69';
+    accommodationPrice: number = 1000;
+    attractionName: string = "Turnul Eiffel";
+    attractionPrice: number = 20;
+
+    constructor(private router: Router) {
+    }
+
     onStartDateSelect(date: NgbDateStruct) {
         this.startDate = date;
     }
 
     onEndDateSelect(date: NgbDateStruct) {
         this.endDate = date;
+    }
+
+    onSaveItinerary() {
+        this.router.navigate('/homepage');
     }
 }
