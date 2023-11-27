59c18728438cbc0adfc5fc01f5ec8a2358c25780
alextopan2341 <alexandru.topan1@stud.ubbcluj.ro>
2023-11-27 22:53:04 +0200

    latest changes in ItineraryController

--- backend/itinerease-backend/src/main/java/com/example/itinereasebackend/api/controller/ItineraryController.java
+++ backend/itinerease-backend/src/main/java/com/example/itinereasebackend/api/controller/ItineraryController.java
@@ -2,10 +2,14 @@ package com.example.itinereasebackend.api.controller;
 
 import com.example.itinereasebackend.api.model.Itinerary;
 import com.example.itinereasebackend.service.ItineraryService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -32,5 +36,28 @@ public class ItineraryController {
     public List<Itinerary> getAll() {
         return itineraryService.getAll();
     }
+
+    @GetMapping("/itinerary/{id}")
+    public ResponseEntity<Object> getItineraryById(@PathVariable int id) {
+        try {
+            Optional<Itinerary> itineraryOptional = itineraryService.getById(id);
+
+            if (itineraryOptional.isPresent()) {
+                Itinerary itinerary = itineraryOptional.get();
+                return ResponseEntity.ok(itinerary);
+            } else {
+                throw new EntityNotFoundException("Itinerary not found");
+            }
+        } catch (EntityNotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+        }
+    }
+
+
+
+
+
 
 }
