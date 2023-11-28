e9f8d95ec14c68c424647c4e533491d115175e78
Sabina <sabina.radu02@gmail.com>
2023-11-28 22:05:33 +0200

    Sign up completed

--- frontend/itinerease-client/src/app/navbar/navbar.component.ts
+++ frontend/itinerease-client/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss',
 })
