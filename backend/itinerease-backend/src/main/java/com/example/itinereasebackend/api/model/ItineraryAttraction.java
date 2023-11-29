7ff90740c55f46efd6e283acafb329971f80fd35
unknown <alexrugina8@yahoo.com>
2023-11-29 21:16:20 +0200

    Attraction cascade delete final

--- backend/itinerease-backend/src/main/java/com/example/itinereasebackend/api/model/ItineraryAttraction.java
+++ backend/itinerease-backend/src/main/java/com/example/itinereasebackend/api/model/ItineraryAttraction.java
@@ -3,6 +3,8 @@ package com.example.itinereasebackend.api.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.io.Serializable;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -10,17 +12,32 @@ import lombok.*;
 @Table(name = "itinerary_attraction", schema = "public")
 public class ItineraryAttraction {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ItineraryAttractionId implements Serializable {
+        @Column(name = "id_itinerary")
+        private int itineraryId;
 
-    @ManyToOne
+        @Column(name = "id_attraction")
+        private int attractionId;
+
+        // Constructori, hashCode, equals, getteri și setteri
+    }
+
+    @EmbeddedId
+    private ItineraryAttractionId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("itineraryId")
     @JoinColumn(name = "id_itinerary", referencedColumnName = "id")
     private Itinerary itinerary;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("attractionId")
     @JoinColumn(name = "id_attraction", referencedColumnName = "id")
     private Attraction attraction;
 
+    // Constructori, getteri și setteri
 }
