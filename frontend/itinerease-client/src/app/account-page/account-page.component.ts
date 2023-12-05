fd431777ae23746cb50d415a980d4987e44c5a88
sandru dairus <darius_sandru@yahoo.com>
2023-12-05 15:39:00 +0200

    latest changes

--- frontend/itinerease-client/src/app/account-page/account-page.component.ts
+++ frontend/itinerease-client/src/app/account-page/account-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-account-page',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './account-page.component.html',
-  styleUrl: './account-page.component.scss'
+    selector: 'app-account-page',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './account-page.component.html',
+    styleUrl: './account-page.component.scss',
 })
-export class AccountPageComponent {
-
-}
+export class AccountPageComponent {}
